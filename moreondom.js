3fce6002d46e5dcf1b307762630c1b4e289012e9
durgesh@gurjar.com <kp7200745@gmail.com>
2025-12-26 21:46:19 +0530

    main

--- moreondom.js
+++ moreondom.js
@@ -2,10 +2,11 @@ var btn = document.querySelector('button')
 var main = document.querySelector('main')
           btn.addEventListener('click',function(){
  var div = document.createElement('h1')
- var arr = ['durgesh','yogesh','ganesh','hariom','patel']
+ var arr = ['I love you Aarti ji😘','I love you Aarti❤️','I love you😍','I love you aayu💕']
+ 
  var x = Math.random()*100
  var y = Math.random()*100
-  var r = Math.random()*360
+  var r = Math.random()*45
  
 
  var c1 = Math.random()*256
@@ -20,7 +21,7 @@ div.style.top = y +'%'
 div.style.rotate = r +'deg'
 div.style.color = `rgb(${c1},${c2},${c3})`
 
-div.style.fontSize = '1.5rem'
+div.style.fontSize = '1.2rem'
 main.appendChild(div)
 
 })
